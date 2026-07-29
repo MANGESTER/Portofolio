00e1999e48924edc572572b87c682236564ade28
e.sherman <herolone83@gmail.com>
2026-07-29 13:23:28 +0700

    just try

---
+++ sistem_atm_sederhana.py
@@ -0,0 +1,15 @@
+saldo = 500000
+
+print("====== Menu ======")
+print("1. Cek Saldo")
+print("2. Tarik Tunai")
+print("3. Setor Tunai")
+print("4. Keluar")
+
+menu = int(input("Pilih menu: "))
+
+if menu == 1:
+    print(f"Saldo anda {saldo}")
+
+elif menu == 4:
+    break

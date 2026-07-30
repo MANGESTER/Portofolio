dfa5f3146f0bda1d07d39387c203b9139a1d7d60
e.sherman <herolone83@gmail.com>
2026-07-30 18:55:24 +0700

    still have a lot bugs

--- sistem_atm_sederhana.py
+++ sistem_atm_sederhana.py
@@ -1,41 +1,33 @@
-# while True:
+while True:
 
-#     saldo_awal = None
+    saldo_awal = None
 
-#     print("-------- Menu --------\n")
-#     print("1. Cek Saldo")
-#     print("2. Tarik Tunai")
-#     print("3. Setor Tunai")
-#     print("4. Keluar")
-#     print("-----------------------")
+    print("-------- Menu --------\n")
+    print("1. Cek Saldo")
+    print("2. Tarik Tunai")
+    print("3. Setor Tunai")
+    print("4. Keluar")
+    print("-----------------------")
 
-#     menu = int(input("Pilih menu: "))
+    menu = int(input("Pilih menu: "))
 
-#     if menu == 1:
-#         print(f"Saldo anda: {saldo_awal}")
+    if menu == 1:
+        print(f"Saldo anda: {saldo_awal}")
 
-#     elif menu == 2:
-#         tarik = int(input("Masukkan nominal: "))
+    elif menu == 2:
+        tarik = int(input("Masukkan nominal: "))
 
-#         if tarik <= saldo_awal:
-#             print("Saldo tidak mencukupi!")
+        if tarik <= saldo_awal:
+            print("Saldo tidak mencukupi!")
 
-#         sisa = saldo_awal - tarik
-#         print(f"Sisa Saldo: {sisa}")
+        sisa = saldo_awal - tarik
+        print(f"Sisa Saldo: {sisa}")
 
-#     elif menu == 3:
-#         setor = int(input("Masukkan nominal: "))
-#         tambah = saldo_awal + setor
-#         print(f"Saldo Anda: {tambah}")
+    elif menu == 3:
+        setor = int(input("Masukkan nominal: "))
+        tambah = saldo_awal + setor
+        print(f"Saldo Anda: {tambah}")
 
-#     elif menu == 4:
-#         print("Program Selesai.")
-#         break 
-
-angka = 2
-
-while angka <= 8:
-    print(angka)
-    angka = angka + 2
-
-print("Selesai")
+    elif menu == 4:
+        print("Program Selesai.")
+        break 
